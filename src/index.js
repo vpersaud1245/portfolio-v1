61879ca73163f1b1f83fda725e80ee10e87d65bb
Vishwanauth Persaud <vpersaud1245@gmail.com>
2024-04-07 17:36:01 -0400

    feat: Add and style job description

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import "./style.css";
 /**
  * TODO
  * Make header slide up on scroll down and slide down on scroll up
- *
+ * Add sliding animation to experience section
+ * Add links to socials and email
+ * Add slide in underlines
  */
